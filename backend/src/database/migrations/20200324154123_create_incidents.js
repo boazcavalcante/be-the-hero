aae13d42eb9ac390e61924fa6c81277a69d3b539
Boaz Cavalcante <boazcavalcante1000@gmail.com>
2020-03-24 20:01:43 -0300

    Back-end adicionado

---
+++ backend/src/database/migrations/20200324154123_create_incidents.js
@@ -0,0 +1,18 @@
+// CREATE INCIDENTS
+exports.up = function(knex) {
+  return knex.schema.createTable('incidents', function(table){
+    table.increments(); // Chave primaria
+
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
+
+    table.string('ong_id').notNullable();
+
+    table.foreign('ong_id').references('id').inTable('ongs'); //Chave estrangeira
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('incidents');
+};
---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const OngController = require('./controllers/ongController');
+const IncidentController = require('./controllers/incidentController');
+const ProfileController = require('./controllers/profileController');
+const SessionController = require('./controllers/sessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+  
+module.exports = routes;
